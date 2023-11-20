e2df64a1057ef67071bf03985e29ad6658af404d
Daniel <danieln311@gmail.com>
2023-11-20 22:57:29 +0100

    added choice class to HTML file + figured out how to clear text field after clicking button again by using a template literal instead of textContent +=

--- js rps.js
+++ js rps.js
@@ -1,18 +1,34 @@
 let choice = ["rock", "paper", "scissors"];
-//create array for the choices of rock, paper, and scissors
 
-let playerSelection = "";
 let computerSelection = "";
-//these variables need to be empty since there is no need for the program to run the variables/functions when the browsers loads the program
+
 let playerScore = 0;
 let computerScore = 0;
 
 console.log(`Player score: ${playerScore}`)
 console.log(`Computer score: ${computerScore}`)
+let gameButtons = document.querySelectorAll(".game-button");
+let playerSelection = "";
+
+gameButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        playerSelection = button.value;
+
+        playGame();
+    })
+})
+
+
+
+
+
+
+
+
 
 function compare(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) { 
-        alert("It\'s a tie, wop wop");
+        console.log("It\'s a tie, wop wop");
     }
     
     else if (playerSelection === "rock" && computerSelection === "scissors") {
@@ -38,32 +54,19 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)]
     //use Math.random to extract a random choice in the choice array to return one of three options
 }
-function getPlayerChoice() {
-    const choice = prompt("What is your answer?");
-    return choice.toLowerCase();
-    //gets the player's input using a prompt, then using toLowerCase to accept case-insensitive input
-}
 
 function playGame() {
-    while (playerScore < 5 && computerScore < 5) {
-        //the while loop will run until either the computer or the player reaches 5 points
-
-
-        //playerSelection and computerSelection declared INSIDE the function so that they do not run OUTSIDE the function more than need
-        let playerSelection = getPlayerChoice()
-        // playerSelection declared and intialized to the function getPlayerSchoice so that when playGame() run THEN the player is prompted to input an answer
-        console.log(playerSelection)
-        //prinot out the playerSelection to the console so that we know what we chose but more importantly to confirm the output is correct
-        let computerSelection = getComputerChoice();
-        console.log(computerSelection)
-        //the same as the comments above
+    let playerChoice = document.querySelector(".player-choice").textContent = `Player Chose: ${playerSelection}`
         
-        compare(playerSelection, computerSelection);
-        // console.log(playerScore);
-        // console.log(computerScore);
-        console.log(`Score: Player: ${playerScore} Computer: ${computerScore}`);
-    }
+    let computerSelection = getComputerChoice();
+    let cpuChoice = document.querySelector(".cpu-choice").textContent = `Computer Chose: ${computerSelection}`
 
+        
+        
+    compare(playerSelection, computerSelection);
+        
+    console.log(`Score: Player: ${playerScore} Computer: ${computerScore}`);
+    
     if (playerScore === 5) {
         console.log("You win!");
     }
@@ -71,11 +74,8 @@ function playGame() {
         console.log("You lost.")
     }
 
-    //this conditional needs top be ran outside of the loop otherwise the conditional would keep running due to the loop
-        
+    
 }
-
-playGame();
 
 
 
